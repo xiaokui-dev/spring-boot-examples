bc73ca1d098f2f1cb2d2db5ee5b88f2e83a24a70
xiaokui.cui <xiaokui.cui@gmail.com>
2020-09-04 19:09:13 +0800

    spring-boot-actuator

---
+++ spring-boot-helloworld/src/test/java/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Description : 单元测试主类
+ *
+ * @author : xiaokui
+ * @date : 2020-08-24
+ */
+@SpringBootTest
+public class ApplicationTests {
+}
